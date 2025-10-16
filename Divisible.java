f36d8765db54ee9721d639ed6e48d56641684c7c
pavani polipalli <polipallipavani54@gmail.com>
2025-10-16 16:15:44 +0530

    Divisible

---
+++ Divisible.java
@@ -0,0 +1,17 @@
+package com.codegnan.operatorexamples;
+import java.util.Scanner;
+public class Divisible {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("enter the number");
+          int num=scanner.nextInt();
+        String result=(num%5==0)?"Divisible by 5":"Not divisible by 5";
+         System.out.println(result);
+         scanner.close();
+	}
+}
+
+
+
